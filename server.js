f3bf500f4cae1e0a70713460b93d3969b9f36a27
Michael A. Chapman <chapmanm305@gmail.com>
2019-05-14 08:51:33 -0700

    Create API call which gets ran through Recipe constructor function - Mike, Jeff, Chloie

--- server.js
+++ server.js
@@ -23,17 +23,55 @@ client.on('error', err => console.error(err));
 // Set view engine for server side templating
 app.set('view engine', 'ejs');
 
+// Constructor
+function Recipe(data) {
+  let count = 1;
+  let amountArr = [];
+
+  while (data[`strIngredient${count}`] !== '') {
+    let ingredient = `strIngredient${count}`;
+    let amount = `strMeasure${count}`;
+    let temp = `${data[amount]} ${data[ingredient]}`;
+    amountArr.push(temp);
+    count++;
+  }
+
+  this.name = data.strMeal;
+  this.instructions = data.strInstructions;
+  this.ingredients = amountArr.join(', ');
+  this.image = data.strMealThumb;
+  this.youtubeLink = data.strYoutube;
+  this.cookbook = 'general';
+}
+
 //Handlers
 let loadHome = (request, response) => {
   response.render('index'); 
-}
+};
+
+let loadSearch = (request, response) => {
+  // get request query
+  let query = request.query.search;
+  let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+  // send API request
+  superagent.get(url)
+    .then(result => {
+      result.body.meals.forEach(meal => {
+        let recipe = new Recipe(meal);
+        console.log(recipe);
+      });
+    });
+  // get results
+  // render search.ejs with response body
+};
 
 let loadAbout = (request, response) => {
   response.render('pages/about'); 
-}
+};
 
 //Routes
 app.get('/', loadHome);
+app.get('/search', loadSearch);
 app.get('/about', loadAbout);
 
 // Error Catcher
